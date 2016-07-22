b38b2d2cfb69e75a6a51127beee1cfcfe289a3e5
Jeff Sommars <sommars1@uic.edu>
2016-07-22 18:08:30 -0500

    Mostly converting to GMP_Integer

--- prevariety_util.h
+++ prevariety_util.h
@@ -9,24 +9,23 @@ namespace Parma_Polyhedra_Library {using IO_Operators::operator<<;}
 class Edge {
 	public:
 		C_Polyhedron cone;
-		list<int> pointIndices;
-		list<int> neighborIndices;
-		list<int> childrenIndices;
-		list<int> parentIndices;
+		list<GMP_Integer> pointIndices;
+		list<GMP_Integer> neighborIndices;
+		list<GMP_Integer> childrenIndices;
+		list<GMP_Integer> parentIndices;
 };
 
 class Facet {
 	public:
-		list<list<int> > Points;
-		C_Polyhedron cone;
-		
+		list<list<GMP_Integer> > Points;
+		Constraint FacetConstraint;
 };
 
 class Hull {
 	public:
 		list<list<GMP_Integer> > Points;
-		map<list<int>,int> PointToIndexMap;
-		map<int,list<int> > IndexToPointMap;
+		map<list<GMP_Integer>,GMP_Integer> PointToIndexMap;
+		map<GMP_Integer,list<GMP_Integer> > IndexToPointMap;
 		list<Edge> Edges;
 		list<Facet> Facets;
 		C_Polyhedron CPolyhedron;
@@ -42,19 +41,31 @@ list<list<GMP_Integer> > GeneratorSystemToPoints(Generator_System gs);
 list<GMP_Integer> ConstraintToPoint(Constraint c);
 
 //------------------------------------------------------------------------------
-Hull NewHull(list<list<int> > Points);
+Hull NewHull(list<list<GMP_Integer> > Points);
 
 //------------------------------------------------------------------------------
-int InnerProduct(list<int> V1, list<int> V2);
+list<Facet> FindFacets(list<list<GMP_Integer> > Points, C_Polyhedron ph);
 
 //------------------------------------------------------------------------------
-list<list<int> > FindInitialForm(list<list<int> > Points, list<int> Vector);
+GMP_Integer InnerProduct(list<GMP_Integer> V1, list<GMP_Integer> V2);
 
 //------------------------------------------------------------------------------
-C_Polyhedron FindCPolyhedron(list<list<int> > Points);
+list<list<GMP_Integer> > FindInitialForm(list<list<GMP_Integer> > Points, list<GMP_Integer> Vector);
+
+//------------------------------------------------------------------------------
+C_Polyhedron FindCPolyhedron(list<list<GMP_Integer> > Points);
 
 //------------------------------------------------------------------------------
 void PrintPoint(list<int> Point);
 
 //------------------------------------------------------------------------------
 void PrintPoint(list<GMP_Integer> Point);
+
+//------------------------------------------------------------------------------
+void PrintPoints(list<list<GMP_Integer> > Points);
+
+//------------------------------------------------------------------------------
+void PrintFacet(Facet F);
+
+//------------------------------------------------------------------------------
+void PrintFacets(list<Facet> Facets);
