3f8c58dda943bbe6fdddfdf2de7721732f8d0d86
Jeff Sommars <sommars1@uic.edu>
2016-07-25 16:46:21 -0500

    Converting lists to vectors

--- cone_intersection.cpp
+++ cone_intersection.cpp
@@ -7,12 +7,12 @@ using namespace std;
 using namespace Parma_Polyhedra_Library;
 namespace Parma_Polyhedra_Library {using IO_Operators::operator<<;}
 
-list<list<list<GMP_Integer> > > CyclicN(int n) {
-	list<list<list<GMP_Integer> > > System;
+vector<vector<vector<GMP_Integer> > > CyclicN(int n) {
+	vector<vector<vector<GMP_Integer> > > System;
 	for (int i = 0; i != n-1; i++) {
-		list<list<GMP_Integer> > Equation;
+		vector<vector<GMP_Integer> > Equation;
 		for (int j = 0; j != n; j++) {
-			list<GMP_Integer> Monomial;
+			vector<GMP_Integer> Monomial;
 			set<GMP_Integer> OneSet;
 			for (int k = 0; k != i+1; k++) {
 				OneSet.insert((j+k)%n);
@@ -31,10 +31,17 @@ list<list<list<GMP_Integer> > > CyclicN(int n) {
 	return System;
 }
 
-int main() {
-	list<Hull> Hulls;
-	list<list<list<GMP_Integer> > > Cyc4 = CyclicN(4);
-	list<list<list<GMP_Integer> > >::iterator CycIt;
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		cout << "Internal error: expected an argument." << endl;
+		return 1;
+	}
+
+	int n = atoi(argv[1]);
+
+	vector<Hull> Hulls;
+	vector<vector<vector<GMP_Integer> > > Cyc4 = CyclicN(n);
+	vector<vector<vector<GMP_Integer> > >::iterator CycIt;
 	for (CycIt=Cyc4.begin(); CycIt != Cyc4.end(); CycIt++) {
 		Hulls.push_back(NewHull(*CycIt));
 	}
@@ -45,51 +52,55 @@ int main() {
 	cout << "Found all hulls." << endl << endl << endl;
 	
 	
-	list<list<C_Polyhedron> > Cones;
-	list<C_Polyhedron> ConeList;
+	vector<vector<C_Polyhedron> > Cones;
+	vector<C_Polyhedron> Conevector;
 	int HullIndex = 0;
 	
-	list<Hull>::iterator it;
+	vector<Hull>::iterator it;
 	for (it=Hulls.begin(); it != Hulls.end(); it++) {
-		list<Edge> Edges = (*it).Edges;
-		list<C_Polyhedron> HullCones;
-		list<Edge>::iterator itr;
+		vector<Edge> Edges = (*it).Edges;
+		vector<C_Polyhedron> HullCones;
+		vector<Edge>::iterator itr;
+		
 		for (itr=Edges.begin(); itr != Edges.end(); itr++) {
-			PrintCPolyhedron((*itr).Cone);
 			HullCones.push_back((*itr).Cone);
+			//PrintCPolyhedron((*itr).Cone);cin.get();
 		};
 		
 		if (HullIndex == 0) {
-			ConeList = HullCones;
+			Conevector = HullCones;
 		} else {
 			Cones.push_back(HullCones);
 		};
 		HullIndex++;
 	};
-	
-	cout << "ConeList count: " << ConeList.size() << endl;
+
+	cout << "Conevector count: " << Conevector.size() << endl;
 	cout << "Cones count: " << Cones.size() << endl;
 	
 	//Iterate through Cones
-	list<list<C_Polyhedron> >::iterator ConesItr;
+	vector<vector<C_Polyhedron> >::iterator ConesItr;
+	int TreeLevel = 1;
 	for (ConesItr=Cones.begin(); ConesItr != Cones.end(); ConesItr++) {
-		list<C_Polyhedron> TestCones = *ConesItr;
-		list<C_Polyhedron> NewCones;
+		vector<C_Polyhedron> TestCones = *ConesItr;
+		vector<C_Polyhedron> NewCones;
 		//Iterate through Cones
-		list<C_Polyhedron>::iterator ConeItr;
-		for (ConeItr=ConeList.begin(); ConeItr != ConeList.end(); ConeItr++) {
+		vector<C_Polyhedron>::iterator ConeItr;
+		for (ConeItr=Conevector.begin(); ConeItr != Conevector.end(); ConeItr++) {
 			//Iterate through TestCones
 			C_Polyhedron Cone = *ConeItr;
-			list<C_Polyhedron>::iterator TestConesItr;
+			vector<C_Polyhedron>::iterator TestConesItr;
 			for (TestConesItr=TestCones.begin(); TestConesItr != TestCones.end(); TestConesItr++) {
 				C_Polyhedron NewCone = IntersectCones(*TestConesItr, Cone);
 				NewCones.push_back(NewCone);
 			};
 		};
-		ConeList = NewCones;
+		Conevector = NewCones;
+		printf("Finished level %d of tree with %d levels\n", TreeLevel, Cones.size());
+		TreeLevel++;
 	};
 	
 	cout << "Finished intersecting C_Polyhedrons." << endl << endl << endl;
-//	PrintCPolyhedrons(ConeList, false);
+//	PrintCPolyhedrons(Conevector, false);
 	
 }
