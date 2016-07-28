930774a8745a826331e60b12c7155a46bdcae6f3
Jeff Sommars <sommars1@uic.edu>
2016-07-28 10:48:06 -0500

    Cleaning up code

--- cone_intersection.cpp
+++ cone_intersection.cpp
@@ -3,13 +3,17 @@
 #include <ctime>
 #include "prevariety_util.h"
 #include <algorithm>
-
 using namespace std;
 using namespace Parma_Polyhedra_Library;
 namespace Parma_Polyhedra_Library {using IO_Operators::operator<<;}
 
-vector<vector<vector<GMP_Integer> > > CyclicN(int n) {
+vector<vector<vector<GMP_Integer> > > CyclicN(int n, bool Reduced) {
 	vector<vector<vector<GMP_Integer> > > System;
+	int Length = n;
+	if (Reduced == true) {
+		Length = n - 1;
+	};
+	
 	for (int i = 0; i != n-1; i++) {
 		vector<vector<GMP_Integer> > Equation;
 		for (int j = 0; j != n; j++) {
@@ -18,7 +22,7 @@ vector<vector<vector<GMP_Integer> > > CyclicN(int n) {
 			for (int k = 0; k != i+1; k++) {
 				OneSet.insert((j+k)%n);
 			};
-			for (int l = 0; l != n; l++) {
+			for (int l = 0; l != Length; l++) {
 				if (OneSet.find(l) != OneSet.end()) {
 					Monomial.push_back(1);
 				} else {
@@ -37,47 +41,42 @@ int main(int argc, char* argv[]) {
 		cout << "Internal error: expected two arguments." << endl;
 		return 1;
 	}
-
 	int n = atoi(argv[1]);
-
+	bool Reduced = true;
 	vector<Hull> Hulls;
-	vector<vector<vector<GMP_Integer> > > Cyc4 = CyclicN(n);
+	vector<vector<vector<GMP_Integer> > > Cyc4 = CyclicN(n,Reduced);
+	if (Reduced == true) {
+		n = n - 1;
+	};
 	vector<vector<vector<GMP_Integer> > >::iterator CycIt;
 	int TestIndex = 0;
 	for (CycIt=Cyc4.begin(); CycIt != Cyc4.end(); CycIt++) {
-		//if (TestIndex == 2) continue;
 		Hulls.push_back(NewHull(*CycIt));
 		TestIndex++;
 	}
-
-	cout << "Hull count: " << Hulls.size() << endl;
-	
-	cout << endl << endl << endl << endl << endl << endl << "Found all hulls." << endl << endl << endl;
-	
+	double ListAppendTime;
+	double ContainmentTime;
 	vector<C_Polyhedron> ConeVector;	
 	
 	if (string(argv[2]) == "refinement") {
+		// Start by initializing the objects.
 		vector<vector<C_Polyhedron> > Cones;
-
 		int HullIndex = 0;
-	
 		vector<Hull>::iterator it;
 		for (it=Hulls.begin(); it != Hulls.end(); it++) {
 			vector<Edge> Edges = (*it).Edges;
 			vector<C_Polyhedron> HullCones;
 			vector<Edge>::iterator itr;
-		
+
 			for (itr=Edges.begin(); itr != Edges.end(); itr++) {
 				HullCones.push_back((*itr).Cone);
 			};
-			
 		
 			if (HullIndex == 0) {
 				ConeVector = HullCones;
 			} else {
 				Cones.push_back(HullCones);
-			};
-			
+			};			
 			HullIndex++;
 		};
 
@@ -99,9 +98,6 @@ int main(int argc, char* argv[]) {
 				vector<C_Polyhedron>::iterator TestConesItr;
 				for (TestConesItr=TestCones.begin(); TestConesItr != TestCones.end(); TestConesItr++) {
 					C_Polyhedron NewCone = IntersectCones(*TestConesItr, Cone);
-					//PrintCPolyhedron(NewCone);
-					//cout << "DIM: " << NewCone.affine_dimension() << endl;
-					//cin.get();
 					IntersectionCount++;
 					if ( find(NewCones.begin(), NewCones.end(), NewCone) == NewCones.end() ) {
 						NewCones.push_back(NewCone);
@@ -113,13 +109,10 @@ int main(int argc, char* argv[]) {
 			TreeLevel++;
 		};
 	}
-	
 	else if (string(argv[2]) == "new") {
 		int ConeIntersectionCount = 0;
-		int HullIndex = 0;	
-		vector<Hull>::iterator it;
-		for (it=Hulls.begin(); it != Hulls.end(); it++) {
-			Hull H = (*it);
+		for (int HullIndex = 0; HullIndex != Hulls.size(); HullIndex++) {
+			Hull H = Hulls[HullIndex];
 			vector<Edge> Edges = H.Edges;
 			vector<Facet> Facets = H.Facets;
 			map<vector<GMP_Integer>,GMP_Integer> PointToIndexMap = H.PointToIndexMap;
@@ -135,80 +128,72 @@ int main(int argc, char* argv[]) {
 				vector<C_Polyhedron> NewCones;
 				vector<C_Polyhedron>::iterator ConeIterator;
 				for (ConeIterator=ConeVector.begin();ConeIterator!=ConeVector.end();ConeIterator++) {
-					//take a random vector from cone
+
 					C_Polyhedron NewCone = *ConeIterator;
 
+					//take a random vector from cone
 					vector<vector<GMP_Integer> > Rays = GeneratorSystemToPoints(NewCone.minimized_generators());
-					//WARNING: this only works for cyclic n!
-					GMP_Integer RandomArray[n];
-					for (int i = 0; i < n; i++) {
-						RandomArray[i] = 0;
-					};
-					vector<vector<GMP_Integer> >::iterator RayIterator;
-					for (RayIterator=Rays.begin();RayIterator != Rays.end(); RayIterator++) {
-						vector<GMP_Integer> Ray = *RayIterator;
-						vector<GMP_Integer>::iterator RayValue;
-						int RayIndex = 0;
-						for (RayValue=Ray.begin();RayValue != Ray.end();RayValue++){
-							RandomArray[RayIndex]+=*RayValue;
-							RayIndex++;
+					vector<GMP_Integer> RandomVector(Rays[0].size(),0);
+					for (int i = 0; i != Rays.size(); i++) {
+						vector<GMP_Integer> Ray = Rays[i];
+						for (int j = 0; j != Ray.size(); j++) {
+							RandomVector[j] += Ray[j];
 						};
 					};
-					
-					//WARNING: this only works for cyclic n!
-					vector<GMP_Integer> RandomVector(RandomArray, + RandomArray + n);
 					
 					//take initial form using that vector.
 					vector<vector<GMP_Integer> > InitialForm = FindInitialForm(Pts, RandomVector);
 					
 					set<GMP_Integer> InitialIndices;
-					vector<vector<GMP_Integer> >::iterator InitialFormItrr;
-					for (InitialFormItrr=InitialForm.begin(); InitialFormItrr != InitialForm.end(); InitialFormItrr++) {
-						InitialIndices.insert(PointToIndexMap[*InitialFormItrr]);
+					for (vector<vector<GMP_Integer> >::iterator InitialFormItr=InitialForm.begin();
+						InitialFormItr != InitialForm.end(); InitialFormItr++)
+					{
+						InitialIndices.insert(PointToIndexMap[*InitialFormItr]);
 					};
-					//explore edge skeleton
-					vector<GMP_Integer> EdgesToTest; //list of indices of edges to visit.
-					vector<Edge>::iterator EdgeItr;
-					GMP_Integer EdgeIndex = 0;
-					for(EdgeItr = Edges.begin(); EdgeItr != Edges.end(); EdgeItr ++) {
-						if (IntersectSets(InitialIndices, (*EdgeItr).PointIndices).size() > 0) {
+					
+					// List of indices of edges to visit.
+					vector<int> EdgesToTest; 
+					for(int EdgeIndex = 0; EdgeIndex != Edges.size(); EdgeIndex++) {
+						if (IntersectSets(InitialIndices, Edges[EdgeIndex].PointIndices).size() > 0) {
 							EdgesToTest.push_back(EdgeIndex);
 						};
-						EdgeIndex++;
 					};
 					
-					vector<GMP_Integer> PretropGraphEdges;
-					vector<GMP_Integer> NotPretropGraphEdges;
-					
+					// Explore edge skeleton
+					set<int> PretropGraphEdges;
+					set<int> NotPretropGraphEdges;
 					while(!EdgesToTest.empty()) {
-						GMP_Integer EToT = EdgesToTest.back();
+						int EdgeToTestIndex = EdgesToTest.back();
 						EdgesToTest.pop_back();
-						GMP_Integer EInd = 0;
-						vector<Edge>::iterator EdgeItr2;
-						Edge EdgeToTest;
-						for(EdgeItr2 = Edges.begin(); EdgeItr2 != Edges.end(); EdgeItr2++) {
-							if (EInd == EToT) {
-								EdgeToTest = *EdgeItr2;
-								break;
+						Edge EdgeToTest = Edges[EdgeToTestIndex];
+						clock_t ContainmentBegin = clock();
+						bool ConeDoesContain = EdgeToTest.Cone.contains(NewCone);
+						ContainmentTime += double(clock() - ContainmentBegin);
+
+						if (ConeDoesContain) {
+							clock_t begin = clock();
+							if ( find(NewCones.begin(), NewCones.end(), NewCone) == NewCones.end() ) {
+								NewCones.push_back(NewCone);
 							};
-							EInd++;
+							ListAppendTime += double(clock() - begin);
+							break;
 						};
 						C_Polyhedron TempCone = IntersectCones(EdgeToTest.Cone, NewCone);
 						ConeIntersectionCount++;
-						//Note: this is why we want to use reduced cyclic n. Consider switching this to if it has any rays.
-						//PrintCPolyhedron(TempCone);
-						//cout << "DIM: " << TempCone.affine_dimension() << endl;
-						//cin.get();
+
 						if (TempCone.affine_dimension() > 0) {
-							PretropGraphEdges.push_back(EToT);
+							PretropGraphEdges.insert(EdgeToTestIndex);
 							
+							clock_t begin = clock();
 							if ( find(NewCones.begin(), NewCones.end(), TempCone) == NewCones.end() ) {
 								NewCones.push_back(TempCone);
 							};	
-							set<GMP_Integer>::iterator NeighborItr;
+							ListAppendTime += double(clock() - begin);
+
+							set<int>::iterator NeighborItr;
+							begin = clock();
 							for(NeighborItr=EdgeToTest.NeighborIndices.begin();NeighborItr!=EdgeToTest.NeighborIndices.end(); NeighborItr++){
-								GMP_Integer Neighbor = *NeighborItr;
-								//if Neighbor not in PretropGraphEdges:
+								int Neighbor = *NeighborItr;
 								if ( find(PretropGraphEdges.begin(), PretropGraphEdges.end(), Neighbor) == PretropGraphEdges.end() ) {
 									if ( find(NotPretropGraphEdges.begin(), NotPretropGraphEdges.end(), Neighbor) == NotPretropGraphEdges.end() ) {
 										if ( find(EdgesToTest.begin(), EdgesToTest.end(), Neighbor) == EdgesToTest.end() ) {
@@ -216,37 +201,31 @@ int main(int argc, char* argv[]) {
 										};
 									};
 								};
-								EdgeToTest.NeighborIndices;
 							};
 						} else {
-							NotPretropGraphEdges.push_back(EToT);
+							NotPretropGraphEdges.insert(EdgeToTestIndex);
 						};
 					};
 				};
 				ConeVector = NewCones;
 			};
 			printf("Finished level %d of tree with %lu levels. %lu cones remain at this level. IntersectionCount = %d.\n", HullIndex, Hulls.size(), ConeVector.size(),ConeIntersectionCount);
-			HullIndex++;
 		};
 		cout << "ConeVector count: " << ConeVector.size() << endl;
 	};
-	
-	
-	
-	
+
 	cout << "Finished intersecting C_Polyhedrons." << endl << endl << endl;
 	vector<Generator> gv;
 	vector<C_Polyhedron>::iterator PolyItr;
 	vector<vector<vector<GMP_Integer> > > InitialForms;
+
+	// This is parsing and displaying all of the pretropisms.
 	for (PolyItr=ConeVector.begin(); PolyItr != ConeVector.end(); PolyItr++) {
 		Generator_System gs = (*PolyItr).minimized_generators();
 		for (Generator_System::const_iterator gsi = gs.begin(),
 		gs_end = gs.end(); gsi != gs_end; ++gsi) {
 			Generator gen = *gsi;
-			if (gen.is_point() or gen.is_line()) {
-				continue;
-			}
-			else if ( find(gv.begin(), gv.end(), gen) == gv.end() ) {
+			if ( find(gv.begin(), gv.end(), gen) == gv.end() ) {
 				
 				vector<GMP_Integer> Pt = GeneratorToPoint(gen);
 				vector<Hull>::iterator HullItr;
@@ -262,15 +241,11 @@ int main(int argc, char* argv[]) {
 					};
 
 					vector<GMP_Integer> PtIndices;
-			//		cout << InForm.size() << endl;
-					
-	
+
 					vector<vector<GMP_Integer> >::iterator InFormIter;
 					for (InFormIter=InForm.begin(); InFormIter!=InForm.end(); InFormIter++) {
 						PtIndices.push_back(MyHull.PointToIndexMap[*InFormIter]);
 					}
-	//				PrintPoints(InForm);
-				//	cout << endl;
 					sort(PtIndices.begin(),PtIndices.end());
 					MyInitialForms.push_back(PtIndices);
 				}
@@ -283,5 +258,7 @@ int main(int argc, char* argv[]) {
 		};
 	};
 	cout << "Number of pretropisms found: " << gv.size() << endl;
-	PrintTime();
+	cout << "Intersection time: " << GetPolyhedralIntersectionTime() / CLOCKS_PER_SEC << endl;	
+	cout << "Containment time: " << ContainmentTime / CLOCKS_PER_SEC << endl;
+	cout << "List append time: " << ListAppendTime / CLOCKS_PER_SEC << endl;
 }
