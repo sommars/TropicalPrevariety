25ec9e714cd378bb3fd4fba932abf4a1b9942160
Jeff Sommars <sommars1@uic.edu>
2016-08-14 17:44:41 -0500

    Checkpoint of work before reverting
    Can't get this code to compile. Ideally, we wouldn't linearly search through all of the nodes; they would be stashed in some reasonable way which would let us access them quickly. Will put this on todo list

--- cone_tree.cpp
+++ cone_tree.cpp
@@ -13,13 +13,6 @@ namespace Parma_Polyhedra_Library {using IO_Operators::operator<<;}
 //The number of constraints varies, but it is >=dimension of the initial problem.
 
 //------------------------------------------------------------------------------
-struct node_compare {
-    bool operator() (const Node& lhs, const Node& rhs) const{
-        return lhs.Generator < rhs.Generator;
-    }
-};
-
-//------------------------------------------------------------------------------
 void InsertCone(Node &Tree, Cone &LeafCone, int HullIndex) {
 	vector<string> GeneratorStrings;
 	// Convert the generators to a list of strings. Sort them by lex
@@ -34,38 +27,21 @@ void InsertCone(Node &Tree, Cone &LeafCone, int HullIndex) {
 	
 	// Bite off p(0).
 	GeneratorStrings.erase(remove(GeneratorStrings.begin(), GeneratorStrings.end(), "p(0)"),GeneratorStrings.end());
-	
 	Node *CurrentRoot = &Tree;
-	for (int i = 0; i != GeneratorStrings.size(); i++) {
-		bool FoundCorrectChild = false;
-		bool IsLastLoop = i+1 == GeneratorStrings.size();
-		vector<Node> *Children = &(*CurrentRoot).Children;
-		for (int j = 0; j != (*Children).size(); j++) {
-			Node *Child = &((*Children)[j]);
-			if ((*Child).Generator == GeneratorStrings[i]) {
-				FoundCorrectChild = true;
-				CurrentRoot = Child;
-				if (IsLastLoop) {
-					(*Child).IsLeaf = true;
-					(*Child).LeafCone = LeafCone;
-					return;
-				};
-				break;
-			};
+	for (size_t i = 0; i != GeneratorStrings.size(); i++) {
+		Node Child;
+		Child.Generator = GeneratorStrings[i];
+		if (i+1 == GeneratorStrings.size()) {
+			Child.IsLeaf = true;
+			Child.LeafCone = LeafCone;
+		} else {
+			Child.IsLeaf = false;
 		};
-		if (!FoundCorrectChild) {
-			Node NewNode;
-			NewNode.Generator = GeneratorStrings[i];
-			// Set the leaf properties correctly, if it is a leaf.
-			if (!IsLastLoop) {
-				NewNode.IsLeaf = false;
-			} else {
-				NewNode.IsLeaf = true;
-				NewNode.LeafCone = LeafCone;
-			};
-			(*CurrentRoot).Children.push_back(NewNode);
-			CurrentRoot = &((*CurrentRoot).Children[(*CurrentRoot).Children.size()-1]);
-		};
+		pair<map<string, Node>::iterator,bool> ret;
+		ret = (*CurrentRoot).Children.insert(pair<string,Node>(GeneratorStrings[i], Child));
+		map<string, Node> *Children = &(*CurrentRoot).Children;
+		
+		CurrentRoot = ((*Children)[GeneratorStrings[i]]);
 	};
 };
 
@@ -74,13 +50,15 @@ vector<Cone> GetCones(Node &Tree) {
 	//TODO: CHANGE GETCONES: make the list at the same time as the tree is made. can't store index because that will get stale. do erase find paradigm.
 
 	vector<Cone> Result;
-	
+	/*
 	if (Tree.IsLeaf) {
 		Result.push_back(Tree.LeafCone);
 	};
-	for (int i = 0; i != Tree.Children.size(); i++) {
-		vector<Cone> NewCones = GetCones(Tree.Children[i]);
+	set<Node>::iterator ChildItr;
+	for (ChildItr = Tree.Children.begin(); ChildItr != Tree.Children.end(); ChildItr++) {
+		Node Child = *ChildItr;
+		vector<Cone> NewCones = GetCones(Child);
 		Result.insert(Result.end(), NewCones.begin(), NewCones.end());
-	};
+	};*/
 	return Result;
 };
