d22996ffaacd9735e0023fa205b21ad3fb419cd2
Jeff Sommars <sommars1@uic.edu>
2016-08-14 14:55:19 -0500

    Switch to using generators instead of constraints

--- cone_tree.cpp
+++ cone_tree.cpp
@@ -12,52 +12,52 @@ namespace Parma_Polyhedra_Library {using IO_Operators::operator<<;}
 //can cut off the children, because it contains its children.
 //The number of constraints varies, but it is >=dimension of the initial problem.
 
+//------------------------------------------------------------------------------
+struct node_compare {
+    bool operator() (const Node& lhs, const Node& rhs) const{
+        return lhs.Generator < rhs.Generator;
+    }
+};
+
+//------------------------------------------------------------------------------
 void InsertCone(Node &Tree, Cone &LeafCone, int HullIndex) {
-	vector<string> ConstraintStrings;
-	// Convert the constraints to a list of strings. Sort them by lex
-	Constraint_System cs = LeafCone.Polyhedron.minimized_constraints();
-	for (Constraint_System::const_iterator i = cs.begin(),
+	vector<string> GeneratorStrings;
+	// Convert the generators to a list of strings. Sort them by lex
+	Generator_System cs = LeafCone.Polyhedron.minimized_generators();
+	for (Generator_System::const_iterator i = cs.begin(),
 	cs_end = cs.end(); i != cs_end; ++i) {
 		stringstream ss;
 		ss << (*i);
-		ConstraintStrings.push_back(ss.str());
+		GeneratorStrings.push_back(ss.str());
 	};
-	sort(ConstraintStrings.begin(),ConstraintStrings.end());
+	sort(GeneratorStrings.begin(),GeneratorStrings.end());
+	
+	// Bite off p(0).
+	GeneratorStrings.erase(remove(GeneratorStrings.begin(), GeneratorStrings.end(), "p(0)"),GeneratorStrings.end());
+	
 	Node *CurrentRoot = &Tree;
-	for (int i = 0; i != ConstraintStrings.size(); i++) {
+	for (int i = 0; i != GeneratorStrings.size(); i++) {
 		bool FoundCorrectChild = false;
+		bool IsLastLoop = i+1 == GeneratorStrings.size();
 		vector<Node> *Children = &(*CurrentRoot).Children;
 		for (int j = 0; j != (*Children).size(); j++) {
 			Node *Child = &((*Children)[j]);
-			if ((*Child).Constraint == ConstraintStrings[i]) {
+			if ((*Child).Generator == GeneratorStrings[i]) {
 				FoundCorrectChild = true;
-				CurrentRoot = &(*Child);
-				if ((*Child).IsLeaf) {
-					// Either the cone we are trying to add is the same cone, or it's smaller.
-					// If it's smaller, we don't want to add it.
-					if (i+1 == ConstraintStrings.size()) {
-						// Intersect all of the pre-intersect indices.
-						for (size_t k = HullIndex; k != LeafCone.IntersectionIndices.size(); k++) {
-							((*Child).LeafCone).IntersectionIndices[k] = IntersectSets(((*Child).LeafCone).IntersectionIndices[k], LeafCone.IntersectionIndices[k]);
-						};
-					};
+				CurrentRoot = Child;
+				if (IsLastLoop) {
+					(*Child).IsLeaf = true;
+					(*Child).LeafCone = LeafCone;
 					return;
-				} else {
-					if (i+1 == ConstraintStrings.size()) {
-						(*Child).IsLeaf = true;
-						(*Child).LeafCone = LeafCone;
-						(*Child).Children.clear();
-						return;
-					};
 				};
 				break;
 			};
 		};
 		if (!FoundCorrectChild) {
 			Node NewNode;
-			NewNode.Constraint = ConstraintStrings[i];
+			NewNode.Generator = GeneratorStrings[i];
 			// Set the leaf properties correctly, if it is a leaf.
-			if (i+1 != ConstraintStrings.size()) {
+			if (!IsLastLoop) {
 				NewNode.IsLeaf = false;
 			} else {
 				NewNode.IsLeaf = true;
@@ -71,16 +71,16 @@ void InsertCone(Node &Tree, Cone &LeafCone, int HullIndex) {
 
 //------------------------------------------------------------------------------
 vector<Cone> GetCones(Node &Tree) {
+	//TODO: CHANGE GETCONES: make the list at the same time as the tree is made. can't store index because that will get stale. do erase find paradigm.
+
 	vector<Cone> Result;
 	
 	if (Tree.IsLeaf) {
 		Result.push_back(Tree.LeafCone);
-	} else {
-		for (int i = 0; i != Tree.Children.size(); i++) {
-			vector<Cone> NewCones = GetCones(Tree.Children[i]);
-			Result.insert(Result.end(), NewCones.begin(), NewCones.end());
-		};
 	};
-	
+	for (int i = 0; i != Tree.Children.size(); i++) {
+		vector<Cone> NewCones = GetCones(Tree.Children[i]);
+		Result.insert(Result.end(), NewCones.begin(), NewCones.end());
+	};
 	return Result;
 };
