0e1b0d1049b5cf420ab6354829f5f99432efe1a1
Jeff Sommars <sommars1@uic.edu>
2016-08-16 09:36:12 -0500

    Removed GMP_Integer

--- prevariety_util.h
+++ prevariety_util.h
@@ -14,20 +14,20 @@ class Cone {
 class Edge {
 	public:
 		Cone EdgeCone;
-		set<GMP_Integer> PointIndices;
+		set<int> PointIndices;
 		set<int> NeighborIndices;
 };
 
 class Facet {
 	public:
-		set<GMP_Integer> PointIndices;
+		set<int> PointIndices;
 		Generator_System Generators;
 };
 
 class Hull {
 	public:
-		vector<vector<GMP_Integer> > Points;
-		map<vector<GMP_Integer>,GMP_Integer> PointToIndexMap;
+		vector<vector<int> > Points;
+		map<vector<int>,int> PointToIndexMap;
 		vector<Edge> Edges;
 		vector<Facet> Facets;
 		C_Polyhedron CPolyhedron;
@@ -46,16 +46,16 @@ C_Polyhedron IntersectCones(C_Polyhedron &ph1, C_Polyhedron &ph2);
 Cone IntersectCones(Cone C1, Cone C2);
 
 //------------------------------------------------------------------------------
-vector<GMP_Integer> GeneratorToPoint(Generator g);
+vector<int> GeneratorToPoint(Generator g);
 
 //------------------------------------------------------------------------------
-vector<vector<GMP_Integer> > GeneratorSystemToPoints(Generator_System gs);
+vector<vector<int> > GeneratorSystemToPoints(Generator_System gs);
 
 //------------------------------------------------------------------------------
-vector<GMP_Integer> ConstraintToPoint(Constraint c);
+vector<int> ConstraintToPoint(Constraint c);
 
 //------------------------------------------------------------------------------
-Hull NewHull(vector<vector<GMP_Integer> > Points);
+Hull NewHull(vector<vector<int> > Points);
 
 //------------------------------------------------------------------------------
 vector<Facet> FindFacets(Hull H);
@@ -64,25 +64,19 @@ vector<Facet> FindFacets(Hull H);
 vector<Edge> FindEdges(Hull H);
 
 //------------------------------------------------------------------------------
-vector<vector<GMP_Integer> > FindCandidateEdges(Hull H);
+vector<vector<int> > FindCandidateEdges(Hull H);
 
 //------------------------------------------------------------------------------
-GMP_Integer InnerProduct(vector<GMP_Integer> V1, vector<GMP_Integer> V2);
+int InnerProduct(vector<int> V1, vector<int> V2);
 
 //------------------------------------------------------------------------------
-vector<vector<GMP_Integer> > FindInitialForm(vector<vector<GMP_Integer> > &Points, vector<GMP_Integer> &Vector);
+vector<vector<int> > FindInitialForm(vector<vector<int> > &Points, vector<int> &Vector);
 
 //------------------------------------------------------------------------------
-C_Polyhedron FindCPolyhedron(vector<vector<GMP_Integer> > Points);
+C_Polyhedron FindCPolyhedron(vector<vector<int> > Points);
 
 //------------------------------------------------------------------------------
-void PrintPoint(vector<int> Point);
-
-//------------------------------------------------------------------------------
-void PrintPoint(vector<GMP_Integer> Point);
-
-//------------------------------------------------------------------------------
-void PrintPoints(vector<vector<GMP_Integer> > Points);
+void PrintPoints(vector<vector<int> > Points);
 
 //------------------------------------------------------------------------------
 void PrintPoint(set<int> Point);
@@ -94,7 +88,7 @@ void PrintCPolyhedron(C_Polyhedron ph, bool PrintIf0Dim = true);
 void PrintCPolyhedrons(vector<C_Polyhedron> phs, bool PrintIf0Dim = true);
 
 //------------------------------------------------------------------------------
-set<GMP_Integer> IntersectSets(set<GMP_Integer> S1, set<GMP_Integer> S2);
+set<int> IntersectSets(set<int> S1, set<int> S2);
 
 //------------------------------------------------------------------------------
-bool SetsDoIntersect(set<GMP_Integer> &S1, set<GMP_Integer> &S2);
+bool SetsDoIntersect(set<int> &S1, set<int> &S2);
