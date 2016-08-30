1fd4290b06346372c59ee1c7d66737c15a83baa5
Jeff Sommars <sommars1@uic.edu>
2016-08-30 08:19:45 -0500

    Switch to NNC_Polyhedron

--- prevariety_util.h
+++ prevariety_util.h
@@ -7,13 +7,14 @@ namespace Parma_Polyhedra_Library {using IO_Operators::operator<<;}
 
 class Cone {
 	public:
-		C_Polyhedron Polyhedron;
+		NNC_Polyhedron Polyhedron;
 		vector<set<int> > IntersectionIndices;
 };
 
 class Edge {
 	public:
 		Cone EdgeCone;
+		bool HasEdgeCone;
 		set<int> PointIndices;
 		set<int> NeighborIndices;
 };
@@ -21,17 +22,16 @@ class Edge {
 class Facet {
 	public:
 		set<int> PointIndices;
-		Generator_System Generators;
 };
 
 class Hull {
 	public:
 		vector<vector<int> > Points;
 		map<vector<int>,int> PointToIndexMap;
+		map<int,vector<int> > IndexToPointMap;
 		vector<Edge> Edges;
 		vector<Facet> Facets;
-		C_Polyhedron CPolyhedron;
-		Generator_System LinealitySpace;
+		NNC_Polyhedron CPolyhedron;
 		int AffineDimension;
 		int SpaceDimension;
 };
@@ -40,7 +40,7 @@ class Hull {
 double GetPolyhedralIntersectionTime();
 
 //------------------------------------------------------------------------------
-C_Polyhedron IntersectCones(C_Polyhedron &ph1, C_Polyhedron &ph2);
+NNC_Polyhedron IntersectCones(NNC_Polyhedron &ph1, NNC_Polyhedron &ph2);
 
 //------------------------------------------------------------------------------
 Cone IntersectCones(Cone C1, Cone C2);
@@ -55,13 +55,16 @@ vector<vector<int> > GeneratorSystemToPoints(Generator_System gs);
 vector<int> ConstraintToPoint(Constraint c);
 
 //------------------------------------------------------------------------------
-Hull NewHull(vector<vector<int> > Points);
+Hull NewHull(vector<vector<int> > Points, bool UseHalfOpenCones);
 
 //------------------------------------------------------------------------------
-vector<Facet> FindFacets(Hull H);
+void FindFacets(Hull &H);
 
 //------------------------------------------------------------------------------
-vector<Edge> FindEdges(Hull H);
+void FindEdges(Hull &H);
+
+//------------------------------------------------------------------------------
+Constraint StrictInequalityToNonStrictInequality(Constraint c);
 
 //------------------------------------------------------------------------------
 vector<vector<int> > FindCandidateEdges(Hull H);
@@ -73,7 +76,10 @@ int InnerProduct(vector<int> V1, vector<int> V2);
 vector<vector<int> > FindInitialForm(vector<vector<int> > &Points, vector<int> &Vector);
 
 //------------------------------------------------------------------------------
-C_Polyhedron FindCPolyhedron(vector<vector<int> > Points);
+NNC_Polyhedron FindCPolyhedron(vector<vector<int> > Points);
+
+//------------------------------------------------------------------------------
+void PrintPoint(vector<int> Point);
 
 //------------------------------------------------------------------------------
 void PrintPoints(vector<vector<int> > Points);
@@ -82,13 +88,10 @@ void PrintPoints(vector<vector<int> > Points);
 void PrintPoint(set<int> Point);
 
 //------------------------------------------------------------------------------
-void PrintPoint(vector<int> Point);
+void PrintCPolyhedron(NNC_Polyhedron ph, bool PrintIf0Dim = true);
 
 //------------------------------------------------------------------------------
-void PrintCPolyhedron(C_Polyhedron ph, bool PrintIf0Dim = true);
-
-//------------------------------------------------------------------------------
-void PrintCPolyhedrons(vector<C_Polyhedron> phs, bool PrintIf0Dim = true);
+void PrintCPolyhedrons(vector<NNC_Polyhedron> phs, bool PrintIf0Dim = true);
 
 //------------------------------------------------------------------------------
 set<int> IntersectSets(set<int> S1, set<int> S2);
